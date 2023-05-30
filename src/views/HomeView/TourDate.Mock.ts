44536bf0e75b15b4385243599229b46a729bf032
Sienvdb <sienvdbergh@gmail.com>
2023-05-30 08:37:39 +0200

    feat: new version data formatted as Sanity data

--- src/views/HomeView/TourDate.Mock.ts
+++ src/views/HomeView/TourDate.Mock.ts
@@ -41,7 +41,56 @@ export const SocialLinksMock = {
 };
 
 export const BioMock = {
-	img: 'https://via.placeholder.com/150',
-	title: 'The Fake Title',
-	content: 'The Fake Content',
+	about: {
+		img: [
+			{
+				_id: 'je id',
+				url: 'https://via.placeholder.com/150',
+			},
+		],
+		bio: [
+			{
+				style: 'normal',
+				_key: 'a4ae38a9c5c2',
+				markDefs: [],
+				children: [
+					{
+						marks: [],
+						text: "John Newman is a multi-faceted DJ, singer, songwriter, and producer creating music on his terms. 2012 smash hit 'Feel The Love' in collaboration with Rudimental first put the British upcoming artist on a trajectory to stardom, going straight in at #1 in the Official UK Charts. One year later, Newman affirmed himself as one of the fasted growing mainstream artists by releasing his debut solo single, 'Love Me Again,' taken from his debut album 'Tribute.' The single became an instant UK #1, charting in the Top 10 in over 30 countries and amassing over 1.6 billion worldwide streams.",
+						_key: 'b9994735e2ea0',
+						_type: 'span',
+					},
+				],
+				_type: 'block',
+			},
+			{
+				_key: 'af0e2925530e',
+				markDefs: [],
+				children: [
+					{
+						_type: 'span',
+						marks: [],
+						text: "Resounding success continued, and in 2014 the x3 BRIT Award nominee teamed up with superstar Calvin Harris on 'Blame', which broke the world record for the most-streamed song in one day and was the first track to ever hit 10 million Spotify streams in less than one week. In more recent times, John's contributions to modern pop culture include a colossal link-up with World #1 DJ and hit producer David Guetta on 'If You Really Love Me (How Will I Know)' and 2023 Tomorrowland Music debut 'Hold On To My Love.' For the first time, John Newman presents himself in a new era as a solo dance music artist.",
+						_key: '405bc085f1a70',
+					},
+				],
+				_type: 'block',
+				style: 'normal',
+			},
+			{
+				markDefs: [],
+				children: [
+					{
+						marks: [],
+						text: "Growing up on a council estate in Northern England, DJing on borrowed gear, Newman's story is about to come full circle. His reign as a pop/soul star saw incredible milestones, including over 2.5 billion streams and nine #1 records as a songwriter and artist. Collaborations with industry leaders such as Sigala, Nile Rogers, Galantis and Kygo. As well as a career-defining performance closing Coachella Festival on stage with Calvin Harris and spectacular live shows at world-class events, EXIT Festival, Glastonbury, and V Festival, to name a few. When the world stopped in 2020, John's lacklustre for creating and the external pressures of the music industry could only be reignited by returning to his dance music roots.",
+						_key: '3c07895c70dc0',
+						_type: 'span',
+					},
+				],
+				_type: 'block',
+				style: 'normal',
+				_key: '2e50f5fd62eb',
+			},
+		],
+	},
 };
