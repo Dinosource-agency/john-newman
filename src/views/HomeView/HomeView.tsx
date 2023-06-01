5ae45a94ba4f882d865df7eecde00485eeea5951
Sienvdb <sienvdbergh@gmail.com>
2023-06-01 16:41:11 +0200

    feat: implement bio component

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -1,8 +1,10 @@
 import { FunctionComponent } from 'preact';
+import { useMemo } from 'preact/hooks';
 import { NavigationLayout } from '~/layouts';
 import { Image } from '~/components/Image/Image';
 import { Heading, TourDate, SocialIcons, Bio } from '~/components';
 import { TourDateMock, SocialLinksMock, BioMock } from './TourDate.Mock';
+import { PortableText } from '@portabletext/react';
 
 export const HomeView: FunctionComponent = () => {
 	return (
@@ -43,20 +45,10 @@ export const HomeView: FunctionComponent = () => {
 					className="a-bio a-bio__image"
 				/>
 				<div class="a-bio">
-					<h5 class="a-bio__h5">Something about me</h5>
-					<p class="a-bio__p">
-						John Newman is a multi-faceted DJ, singer, songwriter,
-						and producer creating music on his terms. 2012 smash hit
-						Feel The Love in collaboration with Rudimental first put
-						the British upcoming artist on a trajectory to stardom,
-						going straight in at #1 in the Official UK Charts. One
-						year later, Newman affirmed himself as one of the fasted
-						growing mainstream artists by releasing his debut solo
-						single, Love Me Again, taken from his debut album
-						Tribute. The single became an instant UK #1, charting in
-						the Top 10 in over 30 countries and amassing over 1.6
-						billion worldwide streams.
-					</p>
+					<h5 class="h5 a-bio__h5">Something about me</h5>
+					<div className="a-bio__p p">
+						<PortableText value={BioMock.about.bio} />
+					</div>
 				</div>
 			</section>
 		</NavigationLayout>
