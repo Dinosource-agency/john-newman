25b2a569e0c15747ac7ec989e93a65ed1b215935
Robbe Bierebeeck <robbebierebeeck@Robbes-MacBook-Pro-6.local>
2023-06-16 12:27:31 +0200

    fix: spacing tour card

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { render } from 'preact';
-import { App } from '~/app.tsx';
-import '~/styles/main.scss';
+import { render } from "preact";
+import { App } from "~/app.tsx";
+import "~styles/main.scss";
 
-render(<App />, document.getElementById('app') as HTMLElement);
+render(<App />, document.getElementById("app") as HTMLElement);
